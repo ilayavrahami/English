a4072e8fbc8563905e058d86037ce6e004f32185
ilayavrahami <165209264+ilayavrahami@users.noreply.github.com>
2024-06-09 21:49:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,132 @@
-const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const row1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const row2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const row3 = ["Z", "X", "C", "V", "B", "N", "M"];
+const letters = [...row1, ...row2, ...row3];
+let currentLetter = "";
+let points = 0; // מספר האותיות הנכונות שהוזנו על ידי המשתמש
 
-let points = 0
-document.getElementById('points').innerHTML = points
+document.getElementById("points").innerHTML = points;
 
-function play(level, test) {
-    if (level == 'new') {
-        resetGame();
-    } else {
-        resetLevel();
-    }
+
+function play(level, letter, test) {
+  if (level == "new") {
+    resetGame();
+    timeLeft = 20;
+  } else {
+    resetLevel();
+  }
+
+  if (letter) currentLetter = letter;
+  else {
     const randomIndex = Math.floor(Math.random() * letters.length);
-    const randomLetter = letters[randomIndex];
-    
-    if (test) document.getElementById('letter').innerText = randomLetter;
+    currentLetter = letters[randomIndex];
+  }
+  if (test) document.getElementById("letter").innerText = currentLetter;
 
-    const wavfile = './audio/' + randomLetter.toUpperCase() + '.wav'; 
-    playAudio(wavfile);
-    
-    window.currentLetter = randomLetter;
+  const wavfile = "./audio/" + currentLetter.toUpperCase() + ".wav";
+  playAudio(wavfile);
+
+  examination(timer);
 }
 
-function examination() {
-    const userInput = document.getElementById('inputLetter').value = document.getElementById('inputLetter').value.toUpperCase();
-    const currentLetter = window.currentLetter;
-    document.getElementById('inputLetter').disabled = true;
-    document.getElementById('resultMessage').hidden = false;
-    if (userInput === currentLetter) {
-        document.getElementById('examination').innerHTML = "נכון מאוד!"
-        console.log(currentLetter);
-        rightAnswer(currentLetter);
-    } else {
-        document.getElementById('examination').innerHTML = "לֹא נָכוֹן. נסה שוב!"
-        wrongAnswer(currentLetter)
-    }
+function examination(timer) {
+  if (timeLeft <= 0) return; // אם הזמן נגמר, אל תבצע את הפונקציה
 
-    play('level');
+  const userInput = document.getElementById("inputLetter").value.toUpperCase();
+  if (!letters.includes(userInput)) return; // אם לא הקיש אות מהרשימה אז אל תעשה כלום
+
+  document.getElementById("resultMessage").hidden = false;
+  if (userInput === currentLetter) {
+    document.getElementById("examination").innerHTML = "נכון מאוד!";
+    rightAnswer(currentLetter);
+    play('level'); // קריאה לפונקציה play() כדי להכין את האות הבאה
+    return; // יציאה מהפונקציה אם התשובה היא נכונה
+  } else {
+    document.getElementById("examination").innerHTML = "לֹא נָכוֹן. נסה שוב!";
+    wrongAnswer(currentLetter);
+    play('level', currentLetter);
+  }
+
+  if (points === 20 || timeLeft <= 0) {
+    // בדיקה האם המשתמש הזין 20 אותיות נכונות או שהזמן נגמר
+    clearInterval(timer); // עצירת הטיימר
+    const score = calculateScore();
+    alert("הניקוד שלך: " + score);
+  }
+}
+
+function calculateScore() {
+  const wrongLettersCount =
+    document.getElementById("wrongLetters").innerHTML.split(",").length - 1;
+  return 20 * points - wrongLettersCount; // חישוב הניקוד על פי מספר האותיות הנכונות והלא נכונות
 }
 
 function rightAnswer(letter) {
-    points++;
-    document.getElementById('points').innerHTML = points;
-    
-    document.getElementById('rightLetters').innerHTML +=  ", " + letter + "-" + letter.toLowerCase();
+  points++;
+  document.getElementById("points").innerHTML = calculateScore();
+
+  document.getElementById("rightLetters").innerHTML +=
+    ", " + letter + "-" + letter.toLowerCase();
 }
 
 function wrongAnswer(letter) {
-    document.getElementById('wrongLetters').innerHTML +=  ", " + letter + "-" + letter.toLowerCase();
+  document.getElementById("wrongLetters").innerHTML +=
+    ", " + letter + "-" + letter.toLowerCase();
 }
 
 function playAudio(audioFile) {
-    const audioElement = document.getElementById('audio');
-    const sourceElement = document.getElementById('audio-source');
-    sourceElement.src = audioFile; 
-    audioElement.load();
-    audioElement.play();
+  const audioElement = document.getElementById("audio");
+  const sourceElement = document.getElementById("audio-source");
+  sourceElement.src = audioFile;
+  audioElement.load();
+  audioElement.play();
 }
 
-function resetLevel(){
-    document.getElementById('inputLetter').value = ""
-    document.getElementById('resultMessage').hidden = true;
-    document.getElementById('inputLetter').disabled = false;
+function resetLevel() {
+  document.getElementById("inputLetter").value = "";
+  document.getElementById("resultMessage").hidden = true;
+  document.getElementById("inputLetter").disabled = false;
 }
 
-function resetGame(){
-    document.getElementById('inputLetter').value = ""
-    document.getElementById('resultMessage').hidden = true;
-    document.getElementById('inputLetter').disabled = false;
+function resetGame() {
+  document.getElementById("inputLetter").value = "";
+  document.getElementById("resultMessage").hidden = true;
+  document.getElementById("inputLetter").disabled = false;
 
-    points = 0
-    document.getElementById('points').innerHTML = points;
+  points = 0;
+  document.getElementById("points").innerHTML = points;
 
-    document.getElementById('rightLetters').value = "";
-    document.getElementById('wrongLetters').value = "";
+  document.getElementById("rightLetters").value = "";
+  document.getElementById("wrongLetters").value = "";
 }
 
-
-const alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-const container = document.getElementById('letterContainer');
-const input = document.getElementById('inputLetter');
-
-alphabet.forEach(letter => {
-  const div = document.createElement('div');
-  div.textContent = letter;
-  div.classList.add('letter');
-  div.addEventListener('click', () => {
-    input.value = letter; // קביעת ערך ה-input לאות הנלחצת
-    examination(); // קריאה לפונקציה examination() ישירות
+const input = document.getElementById("inputLetter");
+const container = document.getElementById("letterContainer");
+[row1, row2, row3].forEach((letters) => {
+  const rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+  letters.forEach((letter) => {
+    const div = document.createElement("div");
+    div.textContent = letter;
+    div.classList.add("letter");
+    div.addEventListener("click", () => {
+      input.value = letter;
+      examination();
+    });
+    rowDiv.appendChild(div);
   });
-  container.appendChild(div);
+  container.appendChild(rowDiv);    
 });
+
+timer = setInterval(() => {
+    timeLeft--;
+    if (timeLeft <= 0) {
+      timeLeft = 0;
+      clearInterval(timer);
+      const score = calculateScore();
+      alert("הניקוד שלך: " + score);
+    } else {
+      document.getElementById("timer").innerText =
+        "זמן נותר: " + timeLeft + " שניות";
+    }
+  }, 1000);
